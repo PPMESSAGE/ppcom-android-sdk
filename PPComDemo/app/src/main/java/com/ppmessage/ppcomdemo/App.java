44ef2f841d6e59fe3bb47cbaa5b6e0dccad7bf5d
PPMessage <kun.zhao@yvertical.com>
2016-07-15 17:58:09 +0800

    local dev

--- PPComDemo/app/src/main/java/com/ppmessage/ppcomdemo/App.java
+++ PPComDemo/app/src/main/java/com/ppmessage/ppcomdemo/App.java
@@ -11,10 +11,15 @@ import com.ppmessage.sdk.core.PPMessageSDK;
  */
 public class App extends Application {
 
-    private static final String TEST_APP_UUID = "914feafd-42a1-11e6-bbfe-58b035f16bf4";
-    private static final String TEST_API_KEY = "MGEzNzFhOTNjMGI2ZjgyMGViYzAzNmI5NTFlYWY2M2IyMTUyODVlZg==";
-    private static final String TEST_API_SECRET = "NjM1MTY4MWU0ZDlmYWEzMzhhZWU4ZjhjNzM5YTJkZDY2N2M0YTRjNA==";
-    private static final String TEST_SERVER_URL = "http://192.168.0.51:8945";
+//    private static final String TEST_APP_UUID = "914feafd-42a1-11e6-bbfe-58b035f16bf4";
+//    private static final String TEST_API_KEY = "MGEzNzFhOTNjMGI2ZjgyMGViYzAzNmI5NTFlYWY2M2IyMTUyODVlZg==";
+//    private static final String TEST_API_SECRET = "NjM1MTY4MWU0ZDlmYWEzMzhhZWU4ZjhjNzM5YTJkZDY2N2M0YTRjNA==";
+//    private static final String TEST_SERVER_URL = "http://192.168.0.51:8945";
+
+    private static final String TEST_APP_UUID = "9c60acbd-44bb-11e6-94c0-acbc327f19e9";
+    private static final String TEST_API_KEY = "YjRlZTYyNWY3ZThjMDJlNDg3YjRkYjNkZDQzNTk0NjdmODk1ZTMzNg==";
+    private static final String TEST_API_SECRET = "MTU3ZWE3MWQ4MTc0NzgxNjRhNGViMTdhMWUyMDUxZTRlYzAzNjg2YQ==";
+    private static final String TEST_SERVER_URL = "http://192.168.0.204:8945";
 
     @Override
     public void onCreate() {
